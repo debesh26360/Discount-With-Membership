8d5d080e52c48a674d28ea1e69e19c0d18797a8c
Debesh Pradhan <51988668+debesh26360@users.noreply.github.com>
2020-12-11 22:18:51 +0530

    Update Discount with Membership 2.py

--- Discount with Membership 2.py
+++ Discount with Membership 2.py
@@ -1,43 +1,30 @@
 total=0
-a=input('enter yes or no for membership')
+a=input('enter yes or no for membership: ')
 if a in ['y','yes','Yes','YES']:
     n=int(input('enter number of items: '))
     for i in range(n):
         cost=int(input('Please enter a item cost:'))
-        if cost>=1000:    
-            print('You have won a discount by 15 percent')
-            cost*=0.85
-            total+=cost
-            print('new cost is ', cost)
-        elif cost<1000:
-            print('You have won a discount by 5 percent')
-            cost*=0.95
-            total+=cost
-            print('new cost is ', cost)
-               
-    print ('Now the total cost is ',total)
-
+        total+=cost
+    if total>=1000:
+        print('Discount of 10%')
+        total*=0.9
+        print('final total is',total)
+    elif total<1000:
+        print('Discount of 5%')
+        total*=0.95
+        print('final total is',total)
 elif a in ['n','no','No','NO']:
     n=int(input('enter number of items: '))
     for i in range(n):
         cost=int(input('Please enter a item cost:'))
-        if cost>=1000:    
-            print('You have won a discount by 5 percent')
-            cost*=0.85
-            total+=cost
-            print('new cost is ', cost)
-        elif cost<1000:
-            print('No discount')
-            cost*=1
-            total+=cost
-            print('new cost is ', cost)
-               
-    print ('Now the total cost is ',total)
-    
+        total+=cost
+    if total>=1000:
+        print('Discount of 5%')
+        total*=0.9
+        print('final total is',total)
+    elif total<1000:
+        print('No discount')
+        total*=0.95
+        print('final total is',total)
 else:
-    print('invalid option')
-
-
-
-
-
+    print('error')
